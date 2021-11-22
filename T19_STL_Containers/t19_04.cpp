14987e45c879d5eb24a6041bc1699f90964045b6
Arsen <avklevtsovskiy@gmail.com>
2021-11-22 17:09:10 +0200

    T19

--- T19_STL_Containers/t19_04.cpp
+++ T19_STL_Containers/t19_04.cpp
@@ -2,7 +2,27 @@
 #include <vector>
 #include <cstdlib>
 #include <ctime>
+#include <algorithm>
 using namespace std;
+
+
+template<class T>
+bool compare(const vector<T>& a, const vector<T>& b) {
+    T maxa = *max_element(a.cbegin(), a.cend());
+    T maxb = *max_element(b.cbegin(), b.cend());
+    return maxa > maxb;
+}
+
+
+template<class T>
+struct comparator {
+
+    bool operator()(const vector<T>& a, const vector<T>& b) {
+        T maxa = *max_element(a.cbegin(), a.cend());
+        T maxb = *max_element(b.cbegin(), b.cend());
+        return maxa > maxb;
+    }
+};
 
 
 vector<vector<int> > random_matrix(int n, int m) {
@@ -29,11 +49,15 @@ ostream& operator<<(ostream& out, const vector<vector<T> >& matrix) {
 int main() {
     srand(time(nullptr));
 
-    int n = 3;
-    int m = 6;
+    int n = 8;
+    int m = 3;
 
     vector<vector<int> > matrix = random_matrix(n, m);
-    cout << matrix;
+    cout << matrix << endl;
+
+    // sort(matrix.begin(), matrix.end(), comparator<int>());
+    sort(matrix.begin(), matrix.end(), compare<int>);
+    cout << matrix << endl;
 
     return 0;
 }
