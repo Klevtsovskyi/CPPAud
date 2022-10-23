2e0292365f414658655f20427bf6b7f2df32fbd2
Arsen <avklevtsovskiy@gmail.com>
2022-10-23 09:34:09 +0300

    T11

--- T11_Binary_files/t11_05.c
+++ T11_Binary_files/t11_05.c
@@ -38,20 +38,104 @@ void print_poly(Poly p) {
 }
 
 
+void print_polynoms(Poly* ps, int n) {
+    for (int i = 0; i < n; i++) {
+        print_poly(ps[i]);
+    }
+}
+
+
+void write_polynoms(Poly* ps, int n, char* filename) {
+    FILE* f = fopen(filename, "wb");
+    if (!f) exit(1);
+
+    fwrite(&n, sizeof (n), 1, f);
+    for (int i = 0; i < n; i++) {
+        fwrite(&ps[i].m, sizeof (int), 1, f);
+        fwrite(ps[i].monoms, sizeof (Monom), ps[i].m, f);
+    }
+    fclose(f);
+}
+
+
+int read_polynoms(Poly* ps, char* filename) {
+    FILE* f = fopen(filename, "rb");
+    if (!f) exit(1);
+
+    int n;
+    fread(&n, sizeof (n), 1, f);
+    for (int i = 0; i < n; i++) {
+        fread(&ps[i].m, sizeof (int), 1, f);
+        fread(ps[i].monoms, sizeof (Monom), ps[i].m, f);
+    }
+    fclose(f);
+    return n;
+}
+
+
+Poly read_poly(int k, char* filename) {
+    FILE* f = fopen(filename, "rb");
+    if (!f) exit(1);
+
+    int n, m;
+    fread(&n, sizeof (n), 1, f);
+    if (k >= n) exit(1);
+
+    for (int i = 0; i < k; i++) {
+        fread(&m, sizeof (int), 1, f);
+        fseek(f, m * sizeof (Monom), SEEK_CUR);
+    }
+    Poly p;
+    fread(&p.m, sizeof (int), 1, f);
+    fread(p.monoms, sizeof (Monom), p.m, f);
+
+    fclose(f);
+    return p;
+}
+
+
+void append_poly(Poly p, char* filename) {
+    FILE* f = fopen(filename, "rb+");
+    if (!f) exit(1);
+
+    int n;
+    fread(&n, sizeof (n), 1, f);
+    n++;
+    rewind(f);
+    fwrite(&n, sizeof (n), 1, f);
+
+    fseek(f, 0, SEEK_END);
+    fwrite(&p.m, sizeof (int), 1, f);
+    fwrite(p.monoms, sizeof (Monom), p.m, f);
+
+    fclose(f);
+}
+
+
 int main() {
+    char F[N] = "F.poly";
     int n = 4;
     int m = 7;
     double arr[N][N] = {
-        {1.0, 2.1, 0.0, 0.0, 0.0, 0.0, -0.1},
+        {1.0, 2.1, 0.1, 3.1, -1.0, 0.0, -0.1},
         {0.0, -9.2, 0.1, 0.0, 3.6, 0.0, 0.0},
         {0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0},
         {0.0, 6.0, 1.0, 0.0, 0.0, 0.0, 0.0}
     };
-    Poly polynoms[N];
+    Poly polynoms_1[N];
     for (int i = 0; i < n; i++) {
-        polynoms[i] = create_poly(arr[i], m);
+        polynoms_1[i] = create_poly(arr[i], m);
     }
-    print_poly(polynoms[0]);
+    write_polynoms(polynoms_1, n - 1, F);
+    append_poly(polynoms_1[3], F);
+
+    Poly polynoms_2[N];
+    n = read_polynoms(polynoms_2, F);
+    print_polynoms(polynoms_2, n);
+    printf("\n");
+
+    Poly p = read_poly(3, F);
+    print_poly(p);
 
     return 0;
 }
